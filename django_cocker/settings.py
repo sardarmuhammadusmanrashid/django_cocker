e702c7e22b6e29379a570cba266aa6fa42c67210
Usman-Rashid <usman.rashid@aitomation.com>
2025-11-30 19:10:18 +0500

    update the changes

--- django_cocker/settings.py
+++ django_cocker/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$1y=j2if3q%75%o_81iomdw&7zy4n)1s=r@5%jy-6nxfu9scm%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
